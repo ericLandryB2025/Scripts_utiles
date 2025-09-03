4e37e2945375ef59ab4431bd908c1c7789cfaee6
yao eric landry badou <202335639@cegepsherbrooke.qc.ca>
2025-09-03 01:19:09 -0400

    correction et ajouter dautres options
    Ajout d'une option pour quitter la calculatrice

--- Scripts pour effectuer les operations de bases.py
+++ Scripts pour effectuer les operations de bases.py
@@ -1,56 +1,51 @@
 # -----------------------------------------------------------------------------
-# Script : lab3_script2.py
+# Script : 
 # Auteur : eric badou
 # Description : ce script permet d’effectuer les 4 opérations de base
 # Paramètres : 
-# Date : 2025-02-19
+# Date : 
 # -----------------------------------------------------------------------------
+
 print("*** calculatrice de base ***")
 print("1-Somme")
 print("2-Soustraction")
 print("3-Multiplication")
 print("4-Division")
+print("5-Quitter")
 
-choix=input("veuillez choisir l'opération a effectuer :")
+choix = input("veuillez choisir l'opération a effectuer :")
 
-if choix =="1" :
-    valeur1=float(input("veuillez entrer le premier chiffre: "))
-    valeur2=float(input("veuillez entrer le deuxieme chiffre: "))
-    somme=valeur1+valeur2
+if choix == "1":
+    valeur1 = float(input("veuillez entrer le premier chiffre: "))
+    valeur2 = float(input("veuillez entrer le deuxieme chiffre: "))
+    somme = valeur1 + valeur2
+    print("Réultat=", somme)
 
-    print("Réultat=",somme)
+elif choix == "2":
+    valeur1 = float(input("veuillez entrer le premier chiffre: "))
+    valeur2 = float(input("veuillez entrer le deuxieme chiffre: "))
+    soustraction = valeur1 - valeur2
+    print("Réultat=", soustraction)
 
-elif choix =="2" :
-    valeur1=float(input("veuillez entrer le premier chiffre: "))
-    valeur2=float(input("veuillez entrer le deuxieme chiffre: "))
-    soustraction=valeur1-valeur2
+elif choix == "3":
+    valeur1 = float(input("veuillez entrer le premier chiffre: "))
+    valeur2 = float(input("veuillez entrer le deuxieme chiffre: "))
+    multiplication = valeur1 * valeur2
+    print("Réultat=", multiplication)
 
-    print("Réultat=",soustraction)
-
-elif choix =="3" :
-    valeur1=float(input("veuillez entrer le premier chiffre: "))
-    valeur2=float(input("veuillez entrer le deuxieme chiffre: "))
-    multiplication=valeur1*valeur2
-
-    print("Réultat=",multiplication)
-
-elif choix == "4" :
-    valeur1=float(input("veuillez entrer le premier chiffre: "))
-    valeur2=float(input("veuillez entrer le deuxieme chiffre: "))
-    if valeur2==0 :
+elif choix == "4":
+    valeur1 = float(input("veuillez entrer le premier chiffre: "))
+    valeur2 = float(input("veuillez entrer le deuxieme chiffre: "))
+    if valeur2 == 0:
         print("il n'est pas possible d'effectuer une division par zéro")
         print("résultat = Pas de résultat")
     else:
-        division=valeur1/valeur2
+        division = valeur1 / valeur2
+        print("Réultat=", division)
 
-    print("Réultat=",division)
+elif choix == "5":
+    print("Merci d'avoir utilisé la calculatrice.")
 
 else: 
     print("l'option choisie n'est pas définie")
     print("résultat = Pas de résultat")
-
-    
-
-    
-    
-
